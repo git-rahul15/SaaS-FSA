9c9a161dcdae6481ed2d28fab59444ce8bc5fd4e
draw.rahul <draw.rahul@gmail.com>
2024-07-12 19:14:38 +0530

    docker-railway

--- src/dfa/urls.py
+++ src/dfa/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('home/', views.home_page_view, name='home'),
+    path("",views.home_page_view, name='main'),
 ]
